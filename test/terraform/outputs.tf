756914f18d65394845df8b06a8a2f24dbb08efd3
pintz <thepintz@gmail.com>
2024-12-03 23:08:09 +0100

    test: adding terraform file for test

---
+++ test/terraform/outputs.tf
@@ -0,0 +1,10 @@
+# Outputs
+output "message1" {
+  value = join(" ", ["-->", var.variable1])
+}
+
+output "message2" {
+  value = join(" ", ["-->", var.variable2])
+}
+
+
---
+++ test/terraform/vars.tf
@@ -0,0 +1,12 @@
+variable "variable1" {
+  type        = string
+  description = "Simple variable 1"
+  default     = "default value for variable 1"
+}
+
+variable "variable2" {
+  type        = string
+  description = "Simple variable 2"
+  default     = "default value for variable 2"
+}
+

62ee1561b967006009eb28039f4f3e8632242612
pintz <61232060+pintz@users.noreply.github.com>
2024-12-09 23:10:55 +0100

    test: adding terraform file for test (#4)

--- terraform/entrypoint.sh
+++ terraform/entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-# entrypoint to launch command PLUGIN_COMMAND 
-terraform "${PLUGIN_COMMAND}"
+#tail -f /dev/null
+
+if [ -z ${PLUGIN_PATH+x} ]
+then 
+	echo "PATH variable is not setted"
+	ls
+	# entrypoint to launch command PLUGIN_COMMAND 
+	terraform "${PLUGIN_COMMAND}"
+else
+	echo "PATH variable is setted to : ${PLUGIN_PATH}"
+	cd ${PLUGIN_PATH}
+	ls
+	terraform "${PLUGIN_COMMAND}"
+fi

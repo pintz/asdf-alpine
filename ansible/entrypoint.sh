f35a7276a2404fc9bb6dac963575884622c62a60
VBRUNET <vincent.brunet@systeme-u.fr>
2024-12-10 18:57:46 +0100

    fix: entrypoint should accept command line in parameter PLUGIN_COMMAND

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-
-#tail -f /dev/null
-
 if [ -z ${PLUGIN_PATH+x} ]
 then 
 	echo "PATH variable is not setted"
-	ls
 	# entrypoint to launch command PLUGIN_COMMAND 
-	"${PLUGIN_COMMAND}"
+	echo -e "#!/usr/bin/bash\n${PLUGIN_COMMAND}" > launcher.sh
+	chmod 755 launcher.sh 
+	./launcher.sh
+
 else
 	echo "PATH variable is setted to : ${PLUGIN_PATH}"
 	cd ${PLUGIN_PATH}
-	ls
-	"${PLUGIN_COMMAND}"
+	echo -e "#!/usr/bin/bash\n${PLUGIN_COMMAND}" > launcher.sh
+	./launcher.sh
+	chmod 755 launcher.sh 
 fi
+

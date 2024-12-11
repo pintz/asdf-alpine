02b8f394270c9fcb94d2838c186323b03494b198
pintz <thepintz@gmail.com>
2024-12-11 23:59:48 +0100

    feat: changing entrypoint for ansible plugin

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ ! -z ${PLUGIN_PRIVATE_KEY+x} ]
+then 
+	echo "SSH_KEY variable is setted"
+	mkdir ${HOME}/.ssh
+	echo -e "${PLUGIN_PRIVATE_KEY}" > ${HOME}/.ssh/id_rsa
+	chmod 400 -R ${HOME}/.ssh
+fi
+
 if [ -z ${PLUGIN_PATH+x} ]
 then 
 	echo "PATH variable is not setted"

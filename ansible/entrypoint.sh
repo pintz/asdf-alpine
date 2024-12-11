d50aa821291ae238564ffb25fcebcbbb082c8807
pintz <thepintz@gmail.com>
2024-12-12 00:36:07 +0100

    adding tests

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -19,7 +19,7 @@ else
 	echo "PATH variable is setted to : ${PLUGIN_PATH}"
 	cd ${PLUGIN_PATH}
 	echo -e "#!/usr/bin/bash\n${PLUGIN_COMMAND}" > launcher.sh
-	./launcher.sh
 	chmod 755 launcher.sh 
+	./launcher.sh
 fi
 
